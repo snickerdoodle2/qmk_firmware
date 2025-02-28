37fcf3d39c1feb242722d3060ece79c29ab6b401
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2025-02-28 18:55:40 +0100

    feat: debugging on

--- keyboards/keychron/v1/ansi_encoder/keymaps/snickerdoodle2/keymap.c
+++ keyboards/keychron/v1/ansi_encoder/keymaps/snickerdoodle2/keymap.c
@@ -59,14 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
 };
 
+#include "print.h"
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (host_keyboard_led_state().caps_lock) {
-        for (uint8_t i = led_min; i < led_max; i++) {
-            if (g_led_config.flags[i] & LED_FLAG_KEYLIGHT) {
-                rgb_matrix_set_color(i, RGB_RED);
-            }
-        }
-    }
+    uprintf("Current layer: %d\n", get_highest_layer(layer_state|default_layer_state));
     return false;
 }
 
